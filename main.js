48ab0b8d8bd704dd834cab82492a4747dc92e54c
Ricardo-Meraz <rmeraz08022005@gmail.com>
2024-06-04 21:22:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function saludar(){
+
+    alert("saludo desde archivo");
+}
+function capturar(){
+
+    edad=prompt("Captura tu edad")
+}
+
+function mostrar(){
+    if(edad>=18){
+        alert("bienvenidos a bodeguas alianza")
+    }else{
+        alert("Notienes edad suficinete para comprar alcohol")
+    }
+
+
+
+
+}
